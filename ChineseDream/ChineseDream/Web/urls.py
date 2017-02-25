556e03b6282bc0ca6990bbd83a5c8062f13532e5
RainCruise <ganyt14@mails.tsinghua.edu.cn>
2017-02-25 20:07:25 +0800

    final version

--- ChineseDream/ChineseDream/Web/urls.py
+++ ChineseDream/ChineseDream/Web/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^about_us.html/$', views.about_us),
     url(r'^join_us_dars.html/$', views.join_us_dars),
     url(r'^join_us_szqt.html/$', views.join_us_szqt),
+    url(r'^join_us_dars_apply/$', views.join_us_dars_apply, name='join_us_dars_apply'),
+    url(r'^join_us_szqt_apply/$', views.join_us_szqt_apply, name='join_us_szqt_apply'),
 ]
